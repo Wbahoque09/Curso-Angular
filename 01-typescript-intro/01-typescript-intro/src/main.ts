68d7129d60c9ad4fc9eaa7a566a29868a86c10e6
Will_09 <wbahoque71@gmail.com>
2023-10-06 16:57:25 -0500

    Se ven desestructuracion de objetos y arreglos

--- 01-typescript-intro/01-typescript-intro/src/main.ts
+++ 01-typescript-intro/01-typescript-intro/src/main.ts
@@ -5,7 +5,8 @@ import './style.css';
 // import '../src/topics/01-basic-types';
 // import '../src/topics/02-object-interface';
 // import '../src/topics/03-functions';
-import '../src/topics/04-homework-types';
+// import '../src/topics/04-homework-types';
+import '../src/topics/05-basic-destructuring';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo
