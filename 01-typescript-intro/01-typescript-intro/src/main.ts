9e0b74381354d74e87b7e38fb67e9023497d3563
Will_09 <wbahoque71@gmail.com>
2023-10-09 17:33:24 -0500

    Se vieron los genericos

--- 01-typescript-intro/01-typescript-intro/src/main.ts
+++ 01-typescript-intro/01-typescript-intro/src/main.ts
@@ -9,7 +9,8 @@ import './style.css';
 // import '../src/topics/05-basic-destructuring';
 // import '../src/topics/06-function-destructuring';
 // import '../src/topics/07-import-export';
-import './topics/08-classes';
+// import './topics/08-classes';
+import './topics/09-generics';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo
