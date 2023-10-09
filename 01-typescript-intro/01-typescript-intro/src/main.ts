73e97830eb97f80bba03dedb3958c7e74829d529
Will_09 <wbahoque71@gmail.com>
2023-10-09 11:23:15 -0500

    Desestructuracion de argumentos y resolucion de tarea hechos

--- 01-typescript-intro/01-typescript-intro/src/main.ts
+++ 01-typescript-intro/01-typescript-intro/src/main.ts
@@ -6,7 +6,8 @@ import './style.css';
 // import '../src/topics/02-object-interface';
 // import '../src/topics/03-functions';
 // import '../src/topics/04-homework-types';
-import '../src/topics/05-basic-destructuring';
+// import '../src/topics/05-basic-destructuring';
+import '../src/topics/06-function-destructuring';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo
---
+++ 01-typescript-intro/01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,46 @@
+
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Xiaomi 12 Pro',
+    price: 110.000,
+}
+
+const tablet: Product = {
+    description: 'Tablet de la CUC',
+    price: 0,
+}
+
+interface TaxCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+const taxCalculation = ( options: TaxCalculationOptions ): [number, number] => {
+
+    const { products, tax } = options;
+    let total = 0;
+
+    products.forEach( ({price}) => {
+        total += price;
+    })
+
+    return [total, total * tax];
+
+}
+
+const shoppingCart = [phone, tablet];
+const tax = 0.19;
+
+const [ total, taxTotal ] = taxCalculation({
+    products: shoppingCart,
+    tax,
+})
+
+console.log(`Total ${total}`);
+console.log(`Tax ${taxTotal}`);
+
+export {};
