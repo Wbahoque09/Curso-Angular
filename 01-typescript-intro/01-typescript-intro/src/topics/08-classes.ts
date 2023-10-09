2daf44dc54781bb9dd9229ccd4185384f0bb7515
Will_09 <wbahoque71@gmail.com>
2023-10-09 16:41:35 -0500

    Priorizacion de la composicion de herencia

--- 01-typescript-intro/01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/01-typescript-intro/src/topics/08-classes.ts
@@ -2,19 +2,56 @@
 
 export class Person {
 
-    public name: string;
-    private address: string;
+    // public name: string;
+    // private address: string;
 
-    constructor( name: string, address: string ) {
+    constructor( 
+        public name: string, 
+        private address: string = 'No Address',
+    ) {
 
-        this.name = name;
-        this.address = address;
+        // this.name = name;
+        // this.address = address;
 
     }
 
 }
 
-const iroman = new Person('Loki','Asgard');
+// export class Hero extends Person {
+
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ) {
+
+//         super( realName, 'Asgard' );
+
+//     } 
+
+// }
+
+export class Hero {
+
+    // public person: Person;
+
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person,
+    ) {
+
+        // this.person = new Person(realName);
+
+    } 
+
+}
+
+const loki = new Person('Loki','Asgard');
+
+// const iroman = new Person('Loki','Asgard');
+const iroman = new Hero('dios del engaño',30,'Loki',loki);
 
 console.log(iroman);
 
