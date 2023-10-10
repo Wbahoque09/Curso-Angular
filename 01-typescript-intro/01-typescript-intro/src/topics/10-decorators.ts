fada89ef3bdd27fc2a008264d3b1ff04a44fe667
Will_09 <wbahoque71@gmail.com>
2023-10-10 11:58:55 -0500

    Se ven decoradores

--- 01-typescript-intro/01-typescript-intro/src/topics/10-decorators.ts
+++ 01-typescript-intro/01-typescript-intro/src/topics/10-decorators.ts
@@ -1,5 +1,26 @@
+function classDecorator<T extends { new (...args:any[]): {} }>(
+    constructor: T
+) {
 
+    return class extends constructor {
+        newProperty = 'Nw';
+        hello = 'hl';
+    }
 
+}
+
+@classDecorator
 export class SuperClass {
     
+    public myProperty: string = 'Abc123';
+
+    print() {
+        console.log('Hola Mundo');
+    }
+
 }
+
+console.log( SuperClass );
+
+const myClass = new SuperClass(); // Instancia de la clase
+console.log( myClass );
