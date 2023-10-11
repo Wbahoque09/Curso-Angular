5b4d545be42485ef973a90da79542a3b3a0c74ff
Will_09 <wbahoque71@gmail.com>
2023-10-10 21:01:31 -0500

    Se practica en el primer mini proyecto de angular, se hacer un contador Component

--- 02-bases/bases/src/app/app.component.ts
+++ 02-bases/bases/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { setupCounter } from '../../../../01-typescript-intro/01-typescript-intro/src/counter';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,14 @@ export class AppComponent {
 
   public title: string = 'Mi primera app de Angular';
   public counter: number = 10;
+
+  increaseBy( value: number ): void {
+    this.counter += value;
+
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
 
 }
---
+++ 02-bases/bases/src/app/counter/counter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({ // Se declara decorador
+  selector: 'app-counter', // El selector es para seleccionar esta clase en otro componente(?)
+  template: '<h1>Hola Counter</h1>', // Template siempre se declara
+})
+export class CounterComponent {
+
+}
