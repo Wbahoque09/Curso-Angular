02e81f50037ab207116b91f5828cf10fa6ef1a97
Will_09 <wbahoque71@gmail.com>
2023-10-11 14:35:27 -0500

    Se empieza a ver la manera de como crear archivos para mostrar en pantalla

--- 02-bases/bases/src/app/app.module.ts
+++ 02-bases/bases/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent, // Se declaran las clases en el arreglo de declarations
+    HeroComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule
