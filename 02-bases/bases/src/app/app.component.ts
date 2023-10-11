c12884e52a78fb470913ad1870283d7ff477890c
Will_09 <wbahoque71@gmail.com>
2023-10-10 21:53:57 -0500

    Se mueve la funcionalidad del contador a otro archivo

--- 02-bases/bases/src/app/app.component.ts
+++ 02-bases/bases/src/app/app.component.ts
@@ -9,15 +9,5 @@ import { setupCounter } from '../../../../01-typescript-intro/01-typescript-intr
 export class AppComponent {
 
   public title: string = 'Mi primera app de Angular';
-  public counter: number = 10;
-
-  increaseBy( value: number ): void {
-    this.counter += value;
-
-  }
-
-  resetCounter(): void {
-    this.counter = 10;
-  }
 
 }
--- 02-bases/bases/src/app/counter/counter.component.ts
+++ 02-bases/bases/src/app/counter/counter.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 
 @Component({ // Se declara decorador
   selector: 'app-counter', // El selector es para seleccionar esta clase en otro componente(?)
-  template: '<h1>Hola Counter</h1>', // Template siempre se declara
+  template: `
+    <h3> Counter: {{ counter }} </h3>
+
+    <button (click)="increaseBy( +1 )" >+1</button>
+    <button (click)="resetCounter()" >Reset</button>
+    <button (click)="increaseBy( -1 )" >-1</button>
+  `, // Template siempre se declara, es literal la parte del html
 })
 export class CounterComponent {
+
+  public counter: number = 10;
+
+  increaseBy( value: number ): void {
+    this.counter += value;
+
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
+  constructor() {}
+
+  ngOnInit() {}
 
 }
