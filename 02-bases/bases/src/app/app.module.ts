ad39e8e8088c5636f47301e305dac929933b3622
Will_09 <wbahoque71@gmail.com>
2023-10-13 14:00:37 -0500

    Se termina la primera seccion de angular completa, se aprende sobre modulos y algunas directivas

--- 02-bases/bases/src/app/app.module.ts
+++ 02-bases/bases/src/app/app.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent, // Se declaran las clases en el arreglo de declarations
-    HeroComponent,
-    ListComponent,
+    // Se declaran las clases en el arreglo de declarations
   ],
-  imports: [
-    BrowserModule
+  imports: [ // Todo lo que termine en Module va en los imports
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
