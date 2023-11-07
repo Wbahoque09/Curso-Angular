e32c45d2258392b8a35ce111334dd2285278ac2c
Will_09 <wbahoque71@gmail.com>
2023-11-07 09:36:32 -0500

    Se termina la parte de servicios, se crean los primeros servicios

--- 02-bases/bases/src/app/dbz/pages/main-page/main-page.component.ts
+++ 02-bases/bases/src/app/dbz/pages/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Character } from '../../interfaces/character.interface';
+import { DbzService } from '../../services/dbz.service';
+
 
 @Component({
   selector: 'app-main-page',
@@ -7,26 +8,7 @@ import { Character } from '../../interfaces/character.interface';
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
-  public characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000,
-    },
-    {
-      name: 'Goku',
-      power: 9500,
-    },
-    {
-      name: 'Vegeta',
-      power: 7500,
-    },
-  ];
 
-  onNewCharacter( character: Character ): void { // Funcion creada para agregar un nuevo elemento al arreglo, se le pasa tipado el parametro
-    this.characters.push(character); // Se agrega al arreglo un nuevo elemento
-  }
+  constructor( public dbzService: DbzService ) {}
 
-  onDeleteCharacter( index: number ): void { // Funcion creada para eliminar un elemento del arreglo
-    this.characters.splice(index,1);
-  }
 }
--- 02-bases/bases/src/app/dbz/services/dbz.service.ts
+++ 02-bases/bases/src/app/dbz/services/dbz.service.ts
@@ -1,7 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
-@Injectable({providedIn: 'root'})
-export class ServiceNameService {
-  constructor() { }
+@Injectable({ providedIn: 'root' })
+export class DbzService {
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500,
+    },
+  ];
 
+  onNewCharacter(character: Character): void {
+    // Funcion creada para agregar un nuevo elemento al arreglo, se le pasa tipado el parametro
+    this.characters.push(character); // Se agrega al arreglo un nuevo elemento
+  }
+
+  onDeleteCharacter(index: number): void {
+    // Funcion creada para eliminar un elemento del arreglo
+    this.characters.splice(index, 1);
+  }
+  constructor() {}
 }
