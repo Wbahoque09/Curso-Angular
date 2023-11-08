1296ab136abf79600eab8ef97b53f97b4a5be1dc
Will_09 <wbahoque71@gmail.com>
2023-11-08 18:02:28 -0500

    Se hacen algunas validaciones

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -14,10 +14,23 @@ export class GifsService {
     return [...this._tagsHistory]; // spread operator creado para hacer copia del arreglo (propiedad) original
   }
 
+  // Metodo creado para organizar y limitar las busquedas
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+
+    this._tagsHistory.unshift(tag); // Aqui insertamos una nueva busqueda
+    this._tagsHistory = this._tagsHistory.splice(0,10); // Aqui limitamos la vista a 10
+
+  }
+
   // Funcion creada para recibir el valor del input y almacenarlo en la propíedad
   searchTag( tag: string ):void {
+    if ( tag.length === 0 ) return;
 
-    this._tagsHistory.unshift( tag );
+    this.organizeHistory(tag);
+
+    // this._tagsHistory.unshift( tag );
 
   }
 
