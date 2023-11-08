0215257b0386fdaf075920df17b7b1dfd96289cc
Will_09 <wbahoque71@gmail.com>
2023-11-08 15:31:09 -0500

    Se ve el decorador viewchild, y su implementacion y tambien sin su implementacion

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.css']
+  styleUrls: ['./search-box.component.css'],
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput') // Decorador que hace referencia a un input en especifico
+  public tagInput!: ElementRef<HTMLInputElement>; //
 
+  constructor() {}
+  // Funcion creada para recibir valor del input
+  // searchTag(newTag: string) {
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value; // constante creada para escuchar el input por medio de el decorador viewchild
+
+    console.log({ newTag });
+  }
 }
