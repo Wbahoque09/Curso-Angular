19d01e7557aa9154a23107c6fd698659bbfcad5f
Will_09 <wbahoque71@gmail.com>
2023-11-16 14:37:52 -0500

    Se termina la seccion de gifss app

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,11 @@ export class GifsService {
   private apiKey: string = 'dt3zyWHTsxR0y2iNpvDueATyFJZD7hBw';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadSessionStorage(); // Se llama al metodo para cargar las busquedas
+    console.log('Carga exitosa');
+
+  }
 
   // Get creado para acceder sin modificar los valores de la propiedad original
   get tagsHistory() {
@@ -30,6 +34,21 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag); // Aqui insertamos una nueva busqueda
     this._tagsHistory = this._tagsHistory.splice(0, 10); // Aqui limitamos la vista a 10
+    this.saveSessionStorage(); // Aqui se llama a la funcion para guardar las busquedas
+  }
+
+  private saveSessionStorage():void { // Metodo o funcion creada para guardar los datos a buscar
+    sessionStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+  }
+
+  private loadSessionStorage(): void { // Metodo o funcion creada para recuperar o traer la informacion en la sessionStorage
+    if (!sessionStorage.getItem('history')) return; // Aqui validamos que el session no este vacio
+
+    this._tagsHistory = JSON.parse( sessionStorage.getItem('history')! ); // Volvemos a cargar las busquedas guardadas
+
+    if ( this._tagsHistory.length === 0 ) return; // Validacion para cargar una data
+    this.searchTag(this._tagsHistory[0]); // Se dispara el metodo de peticiones y se le pasa el primer elemento del arreglo
+
   }
 
   // Funcion creada para recibir el valor del input y almacenarlo en la propíedad
