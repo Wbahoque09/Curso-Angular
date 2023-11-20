7b38fd2802485f1de99cea000f16a75b74b4737f
Will_09 <wbahoque71@gmail.com>
2023-11-20 09:48:45 -0500

    Se inicia el proyecto de countryApp, se deja una estructura base hecha para el diseño de la app

---
+++ 04-countryApp/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule, // Aqui se expone todo lo que el sharedModule va a exponer en toda la app
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
