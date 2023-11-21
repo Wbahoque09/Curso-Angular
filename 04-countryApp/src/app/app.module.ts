06a999d12f818a3245b822deb66840106d72e9da
Will_09 <wbahoque71@gmail.com>
2023-11-21 14:58:16 -0500

    Se muestran los paises en pantalla , se crea un service para la petiicion una interfaz para tipar la respuesta de la peticion

--- 04-countryApp/src/app/app.module.ts
+++ 04-countryApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     BrowserModule,
     SharedModule, // Aqui se expone todo lo que el sharedModule va a exponer en toda la app
+    HttpClientModule, // Se importa para poder usar el HttpClientModule para hacer peticiones en toda la app
   ],
   providers: [],
   bootstrap: [AppComponent],
