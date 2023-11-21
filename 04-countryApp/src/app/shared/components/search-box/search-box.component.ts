b83995aa8aa622f649af6ce4b3248556652a9f02
Will_09 <wbahoque71@gmail.com>
2023-11-21 10:31:52 -0500

    Se termina de implementar el funcionamiento del searchBox

--- 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -10,5 +10,12 @@ export class SearchBoxComponent {
 
   @Input()
   public placeholder: string = '';
+
+  @Output() // Metodo para emitir eventos de un padre hacia el hijo (?)
+   public onListenChild = new EventEmitter<string>(); // Con esto emito eventos desde el padre hacia el hijo (?)
+
+  emitValue( value: string ): void {
+    this.onListenChild.emit( value );
+  }
 
 }
