862a656a5acd8a632ef9622c6172b9703ecbdbb4
Will_09 <wbahoque71@gmail.com>
2023-11-24 16:59:55 -0500

    Se refactoriza la app, terminar seccion y sus funcionalidades

--- 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +9,36 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService,
+  ) {}
+  ngOnInit(): void {
+    // Primera Forma
+    // this.activatedRoute.params
+    // .subscribe( ({id}) => { // Aqui traemos la respuesta
+    //   this.countriesService.searchCountryAlphaCode( id )
+    //   .subscribe( country => { // En este subscribe tenemos el pais despues de esperar la respuesta del anterior subscribe
+    //     console.log({ country });
+    //   })
+    // })
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.countriesService.searchCountryAlphaCode(id) ),
+      )
+      .subscribe( country => {
+        if (!country) {
+          return this.router.navigateByUrl('');
+        }
+
+        console.log('TENEMOS UN PAIS');
+        return;
+      });
+  }
+
+
 
 }
--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Capital } from '../interfaces/capital';
 import { Country } from '../interfaces/country';
 import { Region } from '../interfaces/region';
@@ -31,20 +31,30 @@ export class CountriesService {
       catchError((err) => {
         return of([]);
       })
-    )
+    );
   }
 
   searchRegion(term: string): Observable<Region[]> {
     const url = `${this.apiUrl}/region/${term}`;
     return this.http.get<Region[]>(url).pipe(
       catchError((err) => {
-        return of([])
+        return of([]);
       })
-    )
+    );
+  }
+
+  searchCountryAlphaCode(code: string): Observable<Capital | null> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Capital[]>(url)
+    .pipe(
+      map( countries => countries.length > 0 ? countries[0]: null ),
+      catchError((err) => {
+        return of(null);
+      })
+    );
   }
 
   // searchRegion(term: string): Observable<Country[]> {
 
   // }
-
 }
