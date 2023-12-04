9d34909c2f0f7ca8665f371baf6d6a3cfcd4f56b
Will_09 <wbahoque71@gmail.com>
2023-12-04 11:26:56 -0500

    Se ve la parte de suscripciones

--- 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,13 +1,14 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: [],
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   private debouncer: Subject<string> = new Subject<string>(); // El Subject es un observable que ya contiene los pipes y suscribe (?)
+  private debouncerSuscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -19,7 +20,7 @@ export class SearchBoxComponent implements OnInit {
   public onDebounce = new EventEmitter<string>(); // Con esto emito eventos desde el padre hacia el hijo (?)
 
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSuscription = this.debouncer
       .pipe(
         // Averiguar mas sobre los pipes
         debounceTime(300) // Esto creo que es para dar un tiempo
@@ -27,8 +28,12 @@ export class SearchBoxComponent implements OnInit {
       .subscribe((value) => {
         // El debouncer aqui se encarga de hacer las emiciones
         // console.log('debouncer value', value);
-        this.onDebounce.emit( value );
+        this.onDebounce.emit(value);
       });
+  }
+
+  ngOnDestroy(): void { // Se encarga de destruir components y se ponde debajo del onInit (?)
+    this.debouncerSuscription?.unsubscribe();
   }
 
   emitValue(value: string): void {
