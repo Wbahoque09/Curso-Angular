ba07ec1abcacf6cf03fee7e64f25d834fbbc4efc
Will_09 <wbahoque71@gmail.com>
2024-01-08 18:41:12 -0500

    Se termina la seccion de rutas y lazyloading

--- 06-heroesApp/src/app/app-routing.module.ts
+++ 06-heroesApp/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ), // el loadCh.. es la carga perezosa, es una funcion donde se asigna una promesa.
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule ),
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full' // Esto es para que coincida el path '' al 100%
+  },
+  {
+    path: '**', // Este path es para que cualquiera ruta diferente a las anteriores se redireccione con el redire.. al 404 en este caso
+    redirectTo: '404',
+  }
+]; // Router principal de la app (?)
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
