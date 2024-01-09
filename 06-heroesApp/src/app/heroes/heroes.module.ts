6fc9acfd3d858f829ec2c7047b6e215e9f4a4f19
Will_09 <wbahoque71@gmail.com>
2024-01-09 17:58:09 -0500

    Se adelanta parte de los diseños de layout Heroes

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
@@ -15,11 +17,12 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    MaterialModule,
+    HeroesRoutingModule,
   ]
 })
 export class HeroesModule { }
