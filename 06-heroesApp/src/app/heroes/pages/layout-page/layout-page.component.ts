5f700258770af67bf3a3846f9e697c3c8f72dcd0
Will_09 <wbahoque71@gmail.com>
2024-01-10 09:32:33 -0500

    Se termina el Sidenav e iconos en la page de heroes

--- 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LayoutPageComponent {
-
+  public sidebarItems = [
+    // Items del sidebard creado para añadir iconos(?) Angular Material
+    { label: 'Listado', icon: 'label', url: './list' },
+    { label: 'Añadir', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ];
 }
