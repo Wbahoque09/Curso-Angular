2bef7ddc9ec0c44dd758f06fcd1b2259561d7dac
Will_09 <wbahoque71@gmail.com>
2024-01-11 11:08:52 -0500

    Se crea el componente de card para mostrar los datos de los heroes y se añade en lis-page

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { MaterialModule } from '../material/material.module';
 
+import { CardComponent } from './components/card/card.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
@@ -13,6 +14,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
+    CardComponent,
     HeroPageComponent,
     LayoutPageComponent,
     ListPageComponent,
