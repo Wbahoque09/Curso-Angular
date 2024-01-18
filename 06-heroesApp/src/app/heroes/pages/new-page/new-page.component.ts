476f3e402c950d837ba469ab78085e749e3f7ebb
Will_09 <wbahoque71@gmail.com>
2024-01-18 09:42:47 -0500

    Se introduce de manera mas formal los formulario reactivo en angular

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NewPageComponent {
+  // iMPORTANTE: El FormControl, se importa en este caso en el module de la carpeta, luego se importa en el archivo a utilizar
+  public heroForm = new FormControl({
+    // Se le pasa un objeto con todos los campos del formulario para el control reactivo del mismo
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }), // El nonNullable true es para decirle al formulario que nunca va a llegar nulo
+    publisher: new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego: new FormControl<string>(''),
+    first_appearance: new FormControl<string>(''),
+    characters: new FormControl<string[]>(['']),
+    alt_img: new FormControl(''),
+  });
 
-  public publishers = [ // arreglo para mapear las opciones del select
+  public publishers = [
+    // arreglo para mapear las opciones del select
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
-  ]
-
+  ];
 }
