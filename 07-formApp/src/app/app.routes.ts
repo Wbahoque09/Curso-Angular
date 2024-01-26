714bf60c0f5e99d47b3b2d8285073990f3b7c171
Will_09 <wbahoque71@gmail.com>
2024-01-26 11:29:35 -0500

    Se crea la peticion del login y se crea ruta para el dashboard

--- 07-formApp/src/app/app.routes.ts
+++ 07-formApp/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.routes').then( (m) => m.routes ),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes').then( (n) => n.routes ),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive',
+  },
+];
--- 07-formApp/src/app/reactive/reactive.routes.ts
+++ 07-formApp/src/app/reactive/reactive.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { BasicPageComponent } from './pages/basicPage/basicPage.component';
+import { DynamicPageComponent } from './pages/dynamicPage/dynamicPage.component';
+import { SwitchesPageComponent } from './pages/switchesPage/switchesPage.component';
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'basic',
+        component: BasicPageComponent
+      },
+      {
+        path: 'dynamic',
+        component: DynamicPageComponent
+      },
+      {
+        path: 'switches',
+        component: SwitchesPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'basic',
+      },
+    ]
+  },
+];
