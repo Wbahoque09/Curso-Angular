495e8d15594b1ededc4f48a424c47a78b21516fd
Will_09 <wbahoque71@gmail.com>
2024-02-12 18:28:15 -0500

    Se ven los formularios basicos y su metodo para las validaciones

--- 07-formApp/src/app/reactive/pages/basicPage/basicPage.component.ts
+++ 07-formApp/src/app/reactive/pages/basicPage/basicPage.component.ts
@@ -6,10 +6,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 @Component({
   selector: 'app-basic-page',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './basicPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -23,20 +20,47 @@ export class BasicPageComponent {
   private form = inject(FormBuilder); // Nueva forma de inyectar y no por constructor.
 
   public myForm: FormGroup = this.form.group({
-    name: ['', [ Validators.required, Validators.minLength(3) ]],
-    price: [0, [ Validators.required, Validators.min(0) ]],
-    inStorage: [0, [ Validators.required, Validators.min(0) ]],
-  })
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
 
-  onSave():void {
-
-    if ( this.myForm.invalid ) return;
-
-    console.log(this.myForm.value);
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
   }
 
+  getFieldError( field: string ): string | null {
+    if ( !this.myForm.controls[field] ) return null;
 
- }
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength } caracters.`;
+      }
+    }
+
+    return null;
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
+}
 
 
 /**
