e70a996a98cefed2e0b3d3c28c43b0d11e1b2362
Will_09 <wbahoque71@gmail.com>
2024-03-05 10:35:17 -0500

    Se insertan elementos en el array

--- 07-formApp/src/app/reactive/pages/dynamicPage/dynamicPage.component.ts
+++ 07-formApp/src/app/reactive/pages/dynamicPage/dynamicPage.component.ts
@@ -1,13 +1,93 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './dynamicPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
+
+  // Forma tradicional de hacer un array de campos dinamicos
+  // public myForm2 = new FormGroup({
+  //   favoriteGames: new FormArray([])
+  // })
+
+  // Forma actual (?), FormGroup es para tener agrupados varios campos
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([ // Creo que es para manejar algo dinamico dentro de un formulario
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+  });
+
+  // Se crea para tener control (?) sobre el input dianmico, FormControl es para un solo campo
+  public newFavorite: FormControl = new FormControl('',Validators.required);
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number ) {
+    return formArray.controls[index].errors
+      && formArray.controls[index].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracters.`;
+      }
+    }
+
+    return null;
+  }
+
+  // Se crea funcion para añadir personajes favoritos
+  onAddToFavorites():void {
+    if ( this.newFavorite.invalid ) return;
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(
+      this.fb.control( newGame, Validators.required )
+    );
+
+    this.newFavorite.reset();
+
+  }
+
+  onDeleteFavorite( index:number ): void {
+    this.favoriteGames.removeAt(index);
+
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
+    this.myForm.reset();
+  }
+}
